df47c6f8623a5793560332945449e33a8c5d831e
Milouyg <33333@ma-web.nl>
2025-02-15 20:25:34 +0100

    Made a start with the contact at the Welcome page

--- mbohub/resources/js/Pages/Welcome.jsx
+++ mbohub/resources/js/Pages/Welcome.jsx
@@ -30,12 +30,46 @@ Wat doen we? We verbinden bedrijven en organisaties met mbo-studenten, die meewe
                 <h1 className="projects-h1"> Projects</h1>
                 <ul className="project-container">
                     <Project labelText={"Dit is een label"}/>
-                    <Project labelText={"Dit is een label"}/>
+                    <Project labelText={"Dit is een test"}/>
                 </ul>
             </section>
 
             {/* Footer Section */}
-            <footer className="site-footer">
+            <footer className="footer">
+                <div className="footer-container">
+                    <div className="footer-contact">
+                        <h2 className="footer-title">Contact</h2>
+                        &#x1F4E7;<span className="footer-email">info@mbo-hub.amsterdam</span>
+                        <p className="footer-text">Ben je benieuwd wat mbo-studenten voor jouw project kunnen betekenen? Neem contact met ons op!</p>
+                    </div>
+                    <div className="footer-links">
+                    <h2 className="footer-title">Links</h2>
+                        <ul className="links-container">
+                            <li className="link">Over Ons</li>
+                            <li className="link">Projecten</li>
+                            <li className="link">Contact</li>
+                            <li className="link">Privacy Policy</li>
+                        </ul>
+                    </div>
+                    <div className="footer-socials">
+                        <h2 className="footer-title">Volg ons</h2>
+                        <ul className="socials-container">
+                            <li className="social">
+                                <a className="a" href="https://www.linkedin.com/company/mbo-hub-amsterdam/?">Linkedin</a>
+                            </li>
+                            <li className="social">
+                                <a className="a" href="https://www.instagram.com/mbohub.amsterdam/">Instagram</a>
+                            </li>
+                            <li className="social">
+                                <a className="a" href="https://www.youtube.com/@MBO-Hub">Youtube</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <figure className="line"></figure>
+                <h2 className="footer-btm-title">© 2025 MBO-Hub Amsterdam. Alle rechten voorbehouden.</h2>
+            </footer>
+            {/* <footer className="site-footer">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-4">
@@ -87,7 +121,7 @@ Wat doen we? We verbinden bedrijven en organisaties met mbo-studenten, die meewe
                         </div>
                     </div>
                 </div>
-            </footer>
+            </footer> */}
         </GuestLayout>
     );
 }
