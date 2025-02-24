0401f32ffdb488889310b6673f4ce1fa6a1700b4
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-02-24 13:37:38 +0100

    added calender

---
+++ mbohub/resources/js/Components/calender.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function Calender(){
+    return(
+        <>
+            <article className="calender">
+            <h2>Kalender</h2>
+                <ul className="calender__dates">
+                    <li className="calender__activity">
+                        <figure className="calender__date">
+                            <span>10 Maart</span>
+                        </figure>
+                        <div></div>
+                        <section className="calender__activities">
+                            <div className="calender__header">
+                                <h2 className="calender__title">Workshop UX/UI Design</h2>
+                                <span className="calender__label">
+                                    excursie
+                                </span>
+                            </div>
+                            <p className="calender__text">zaterdag 22 februari 2025</p>
+                            <p className="calender__text">Locatie: B.Amsterdam</p>
+                            <h3 className="calender__subHeading">Bezoek aan verschillende innovatieve startups in Amsterdam.</h3>
+                        </section>
+                    </li>
+                    <li className="calender__activity">
+                        <figure className="calender__date">
+                            <span>10 Maart</span>
+                        </figure>
+                        <div></div>
+                        <section className="calender__activities">
+                            <div className="calender__header">
+                                <h2 className="calender__title">Workshop UX/UI Design</h2>
+                                <span className="calender__label">
+                                    excursie
+                                </span>
+                            </div>
+                            <p className="calender__text">zaterdag 22 februari 2025</p>
+                            <p className="calender__text">Locatie: B.Amsterdam</p>
+                            <h3 className="calender__subHeading">Bezoek aan verschillende innovatieve startups in Amsterdam.</h3>
+                        </section>
+                    </li>
+                </ul>
+            </article>
+        </>
+    )
+}
--- mbohub/resources/js/Pages/Welcome.jsx
+++ mbohub/resources/js/Pages/Welcome.jsx
@@ -1,22 +1,12 @@
 import GuestLayout from "@/Layouts/GuestLayout";
 import Project from "./project-component";
 import { Banner } from "@/Layouts/banner";
+import Calender from "@/Components/calender";
 
 function Welcome() {
     return (
         <GuestLayout hideNavigation={false}>
             <Banner />
-
-            {/* Events Section */}
-            <section className="section events">
-                <h2>Welkom bij MBO-Hub!</h2>
-                <p>MBO-Hub Amsterdam is van start! MBO-Hub Amsterdam is een samenwerking tussen ROC van Amsterdam, Mediacollege Amsterdam en HMC, opgericht in het kader van Amsterdam 750. Wij zijn dé plek waar mbo-studenten, bedrijven, de Gemeente Amsterdam en initiatieven samenkomen om te werken aan de toekomst van onze stad. De MBO-Hub biedt studenten een kans om hun talenten in de praktijk te brengen, door ze te betrekken bij projecten, evenementen en initiatieven rondom het 750-jarig jubileum van Amsterdam.
-
-Wat doen we? We verbinden bedrijven en organisaties met mbo-studenten, die meewerken aan diverse opdrachten zoals evenementenorganisatie, videoproducties, groenprojecten en nog veel meer. Hierdoor doen de studenten waardevolle praktijkervaring op, door middel van stages, projecten en leerwerkopdrachten, en dragen ze bij aan de ontwikkeling van Amsterdam. Belang van de MBO-Hub Mbo-studenten vormen het kloppende hart van de stad. Door hen te betrekken bij belangrijke projecten zorgen we voor nieuwe ideeën, jonge energie en een sterkere verbinding tussen onderwijs en de maatschappij.</p>
-            <div className="oberpop">
-            <img src="/img/poppetjeober-removebg-preview.png" alt="MBO Hub character" />
-            </div>
-            </section>
 
             {/* Projects Section */}
 
@@ -27,6 +17,8 @@ Wat doen we? We verbinden bedrijven en organisaties met mbo-studenten, die meewe
                     <Project topLabelText={"Dit is een label"} bottomLabelText={"Dit is een label"} />
                 </ul>
             </section>
+
+            <Calender />
 
             {/* Footer Section */}
             <footer className="site-footer">
