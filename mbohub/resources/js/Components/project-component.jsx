6c1e44272787e6b3b173f3b5bc782edcb428d18e
Milouyg <33333@ma-web.nl>
2025-02-27 11:10:46 +0100

    fixed styling of project-component

--- mbohub/resources/js/Components/project-component.jsx
+++ mbohub/resources/js/Components/project-component.jsx
@@ -62,7 +62,7 @@ export function Project({ title, text, topLabelText, bottomLabelText, date, read
     );
 }
 
-export function Modal({ title, text, date, onClose, button}) {
+export function Modal({ title, text, date, onClose, button }) {
     return (
         <>
             <div className="model__background">
@@ -70,14 +70,14 @@ export function Modal({ title, text, date, onClose, button}) {
                     <button className="modal-close" onClick={onClose}>X</button>
                     <div className="modal-content">
                         <div className="modal-content-top">
-                            <h2 className="modal-header">{ title }</h2>
+                            <h2 className="modal-header">{title}</h2>
                             <datetime className="modal-date" datetime="">{date}</datetime>
                         </div>
                         <figure className="modal-content-img">
                             <img className="modal-img" src="https://placehold.co/400x200" alt="" />
                         </figure>
                         <div className="modal-content-bottom">
-                            <p className="modal-p">{ "lorem ipsum. Lorem ipsum lorem ipsum lorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsum" }</p>
+                            <p className="modal-p">{"lorem ipsum. Lorem ipsum lorem ipsum lorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsumlorem ipsum. Lorem ipsum lorem ipsum"}</p>
                             {button && (
                                 <button className="modal-btn">Bekijk het verhaal</button>
 
