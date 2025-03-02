b65fa9439d4fa6f3db0c2eb2425be615020d9fc1
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-03-02 12:57:15 +0100

    added base react

--- mbohub/resources/js/Layouts/Navigation.jsx
+++ mbohub/resources/js/Layouts/Navigation.jsx
@@ -1,19 +1,25 @@
 import { usePage } from "@inertiajs/react";
 import Navlink from '../Components/NavLink';
+import React, { useState } from 'react';
 
 function Navigation() {
     const { auth } = usePage().props;
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const handleNavToggle = () => {
+        setIsNavOpen(!isNavOpen);
+    };
 
     return (
         <header className="header">
             <a href="#">
-            <figure className="header__hubLogo">
-                <img
-                    src="/img/MBO-Hub-logo.webp"
-                    alt="Logo"
-                    className="header__logo"
-                />
-            </figure>
+                <figure className="header__hubLogo">
+                    <img
+                        src="/img/MBO-Hub-logo.webp"
+                        alt="Logo"
+                        className="header__logo"
+                    />
+                </figure>
             </a>
             <nav className="nav">
                 <Navlink href={route('projects.projects')}>Projects</Navlink>
@@ -33,6 +39,24 @@ function Navigation() {
                         <span className="guest-name">Guest</span>
                     )}
                 </div> */}
+                <button className="menu__button" onClick={handleNavToggle}>
+                    {isNavOpen ? (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                        </svg>
+                    ) : (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+                        </svg>
+                    )}
+                </button>
+                <section className="menu">
+                    <Navlink href={route('projects.projects')}>Projects</Navlink>
+                    <Navlink href={route('about.about')}>About</Navlink>
+                    <Navlink href={route('partners.partners')}>Partners</Navlink>
+                    <Navlink href={route('contact.contact')}>Contact</Navlink>
+                    <Navlink href={route('login')}>Login</Navlink>
+                </section>
             </nav>
         </header>
     );
