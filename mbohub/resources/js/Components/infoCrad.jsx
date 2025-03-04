e62cdc37aac8e10ca6417f0ac48e635b3d275440
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-03-04 09:33:56 +0100

    info responsive

--- mbohub/resources/js/Components/infoCrad.jsx
+++ mbohub/resources/js/Components/infoCrad.jsx
@@ -17,7 +17,7 @@ export default function InfoCard() {
             </li>
             <li className="card__card">
                 <a href="" className="card__link">
-                    <h3 className="card__title">Nieuws</h3>
+                    <h3 className="card__title">Kalender</h3>
                     <p className="card__paragraph">Ontdek ons team en de toonaangevende partners waarmee we samenwerken om innovatie in het MBO mogelijk te maken.</p>
                 </a>
             </li>
