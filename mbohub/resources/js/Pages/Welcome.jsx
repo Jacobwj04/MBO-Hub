1817994352ee717a244f49c7605586d6d3f2186e
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-03-04 11:57:45 +0100

    removed one projects import

--- mbohub/resources/js/Pages/Welcome.jsx
+++ mbohub/resources/js/Pages/Welcome.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import GuestLayout from "@/Layouts/GuestLayout";
 import { Project, Modal } from "../Components/project-component";
-import Project from "../Components/project-component";
 import FooterComponent from "./Contact/footer";
 import { Banner } from "@/Layouts/banner";
 import Calender from "@/Components/calender/calender";
