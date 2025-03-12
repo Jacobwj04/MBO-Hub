070df2ba6ad87dbfe2e522ff97065183629aef93
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-03-12 11:35:15 +0100

    fixed login page

--- mbohub/resources/js/Pages/Auth/Login.jsx
+++ mbohub/resources/js/Pages/Auth/Login.jsx
@@ -1,8 +1,3 @@
-import Checkbox from '@/Components/Checkbox';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
 import { Head, Link, useForm } from '@inertiajs/react';
 
 export default function Login({ status, canResetPassword }) {
@@ -21,78 +16,22 @@ export default function Login({ status, canResetPassword }) {
 	};
 
 	return (
-		<div>
+		<>
 			<Head title="Log in" />
-
-			{ status && (
-				<div className="mb-4 text-sm font-medium text-green-600">
-					{ status }
-				</div>
-			) }
-
-			<form onSubmit={ submit }>
-				<div className="container--email">
-					<InputLabel htmlFor="email" value="Email" className="title--email" />
-
-					<TextInput
-						id="email"
-						type="email"
-						name="email"
-						value={ data.email }
-						className="mt-1 block w-full"
-						autoComplete="username"
-						isFocused={ true }
-						onChange={ (e) => setData('email', e.target.value) }
-					/>
-
-					<InputError message={ errors.email } className="mt-2" />
-				</div>
-
-				<div className="container--password">
-					<InputLabel htmlFor="password" value="Password" className="title--password" />
-
-					<TextInput
-						id="password"
-						type="password"
-						name="password"
-						value={ data.password }
-						className="mt-1 block w-full"
-						autoComplete="current-password"
-						onChange={ (e) => setData('password', e.target.value) }
-					/>
-
-					<InputError message={ errors.password } className="mt-2" />
-				</div>
-
-				<div className="form__input">
-					<label className="form__onthoud">
-						<Checkbox
-							name="remember"
-							checked={ data.remember }
-							onChange={ (e) => setData('remember', e.target.checked) }
-						/>
-						<span className="remember--text" style={ { fontSize: '0.75rem', lineHeight: '1.2' } }>
-    Remember me
-</span>
-
-					</label>
-				</div>
-
-				<div className="form__forgot form__input">
-					{ canResetPassword && (
-						<Link
-							href={ route('password.request') }
-							className="text--for"
-						>
-							Forgot your password?
-						</Link>
-					) }
-
-					<PrimaryButton className="ms-4" disabled={ processing }>
-						Log in
-					</PrimaryButton>
-				</div>
-			</form>
-		</div>
+			<section className="calenderEdit">
+				<form onSubmit={submit} className="calenderEdit__form">
+					<input type="email" id="email" name="email" value={data.email} className="calenderEdit__input" autoComplete="username" onChange={(e) => setData('email', e.target.value)} placeholder='Vul uw Email in' />
+					<input type="empasswordail" id="password" name="password" value={data.password} className="calenderEdit__input" autoComplete="current-password" onChange={(e) => setData('password', e.target.value)} placeholder='Vul uw wachtwoord in' />
+					<span>
+						<input type="checkbox" id="remember" name="remember" checked={data.remember} className="calenderEdit__input--checkbox" onChange={(e) => setData('remember', e.target.checked)} />
+						<label htmlFor="remember">Remember me</label>
+					</span>
+					{canResetPassword && (
+						<a href={route('password.request')}>Forgot your password?</a>
+					)}
+					<input type="submit" value="Verstuur" className="calenderEdit__submit" />
+				</form>
+			</section>
+		</>
 	);
 }
